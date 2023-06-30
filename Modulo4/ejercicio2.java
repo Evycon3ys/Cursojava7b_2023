f4065f794b98d802f48986038cf6e08bbf3c62e6
unknown <Ochoamerchanjulieta@gmail.com>
2023-06-29 21:26:13 -0300

    ejercicios

---
+++ Modulo4/ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+	
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número entero:");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número ingresado es par.");
+        } else {
+            System.out.println("El número ingresado es impar.");
+        }
+    }
+}
+
